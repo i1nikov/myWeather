1d05a5e829e8777ffdb56e08e44f2a330dddf608
i1nikovv <i1nikov@i.ua>
2019-06-04 14:29:57 +0300

    second commit
    second

--- js/index.js
+++ js/index.js
@@ -1,20 +1,25 @@
-var  exchange=document.getElementById('exchange');
-var  buy=document.getElementById('buy');
-var  sale=document.getElementById('sale');
-function getData (){ 
+var thisCity= document.getElementById('thisCity');
+ navigator.geolocation.getCurrentPosition(function (position){
+ var lat = position.coords.latitude;
+ var lng = position.coords.longitude;
+
+ function getThisCity (){
+    var proxy = "https://cors-anywhere.herokuapp.com/";
     var xhttp = new XMLHttpRequest();
-    xhttp.open("GET",'https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5');
+ xhttp.open("GET",`${proxy}http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&APPID=ae0f2116b81c9b8d32f1d370cfecd486`); 
     xhttp.send();
-    xhttp.addEventListener('readystatechange',getValute);
- 
-    function getValute(){
+    xhttp.addEventListener('readystatechange',getCity);
+
+    function getCity(){
         if(xhttp.status===200 && xhttp.readyState===4){   
-          var valute=JSON.parse(xhttp.responseText);
-          console.log(valute)   
-  exchange.innerHTML="<br>"+valute[0].ccy+"<br>"+valute[1].ccy+"<br>"+valute[2].ccy;
-  buy.innerHTML="Покупка"+"<br>"+valute[0].buy+"<br>"+valute[1].buy+"<br>"+valute[2].buy;        
-sale.innerHTML="Продажа"+"<br>"+valute[0].sale+"<br>"+valute[1].sale+"<br>"+valute[2].sale;               
-        }    
+          var data=JSON.parse(xhttp.responseText);
+          console.log(data)
+        
+          
+thisCity.innerHTML="<b>YOUR CITY</b>"+"<br>"+'<img src="https://openweathermap.org/img/w/'+data.weather[0].icon+'.png">'+"<br>"+Math.round(data.main.temp-273)+"℃"+"<br>"+data.name+","+data.weather[0].description;
+           }
       }
-}
-getData();
+
+}  
+  getThisCity ()
+ });
